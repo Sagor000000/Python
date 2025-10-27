b577918e08a946a13e77ce29aecc5cb49aeeb47e
Habibur Rahman Sagor <hrsagor34@gmail.com>
2025-10-27 10:57:47 +0600

    Add files via upload

---
+++ if_else.py
@@ -0,0 +1,9 @@
+a = 78
+b = 45
+c = 89
+if a > b and a > c:
+    print("a is the largest number")
+elif b > a and b > c:
+    print("b is the largest number")
+else:
+    print("c is the largest number")
